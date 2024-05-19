ab773c1c1e39d8069ee9e0aa2cc24d69267c6c67
Baleria Reyes <breyes815@gmail.com>
2024-05-19 12:27:51 -0700

    user model export

--- models/users.js
+++ models/users.js
@@ -6,7 +6,18 @@ const User = sequelize.define('user', {
   id: { type: DataTypes.INTEGER, allowNull: false},
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
+  password: { 
+    type: DataTypes.STRING, 
+    allowNull: false
+  },
   admin: { type: DataTypes.BOOLEAN, defaultValue: false }
 })
+
+exports.User = User
+exports.UserClientFields = [
+  'id',
+  'name',
+  'email',
+  'admin'
+]
 
